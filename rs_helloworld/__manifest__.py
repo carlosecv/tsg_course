0639b9e949ef5426c594ae28bc4da21b0006f5ca
Carlos Contreras <carlosecv@realsystems.com.mx>
2026-02-10 17:37:28 -0600

    [IMP] First manifest file declaration
    We declare a dictionary for our odoo module
    
    {
        'name': 'Hello World Module', # Human readable description
        'summary': 'This is a demostration First Module', #Short Description for my module
        'version': '1.0', # Version
        'description': """
        """, # Long Descritpion of my module in markup syntax
        'author': 'Real Systems', who id this
        'maintainer': 'Carlos Contreras <carlosecv@realsystems.com.mx>', who code this
        'contributors': [], other contributions separated by coma
        'website': 'http://www.github.com/carlosecv/tsg_course.git',
    
        'license': 'AGPL-3',
        'category': 'Customization',
        'depends': [
            'base'
        ], Wich modules depends
        'external_dependencies': {
            'python': [
            ],
        }, # Which dependencys python modules
        'application': False,  # True or False
        'installable': True  # Its instalable or not
    }

---
+++ rs_helloworld/__manifest__.py
@@ -0,0 +1,45 @@
+
+{
+    'name': 'Hello World Module',
+    'summary': 'This is a demostration First Module',
+    'version': '1.0',
+
+    'description': """
+Hello World Module.
+==============================================
+  * This is a demostration First Module
+
+    """,
+
+    'author': 'Real Systems',
+    'maintainer': 'Carlos Contreras <carlosecv@realsystems.com.mx>',
+    'contributors': [],
+    'website': 'http://www.github.com/carlosecv/tsg_course.git',
+
+    'license': 'AGPL-3',
+    'category': 'Customization',
+
+    'depends': [
+        'base'
+    ],
+    'external_dependencies': {
+        'python': [
+        ],
+    },
+    'data': [
+    ],
+    'demo': [
+    ],
+    'js': [
+    ],
+    'css': [
+    ],
+    'qweb': [
+    ],
+    'images': [
+    ],
+    'test': [
+    ],
+    'application': False,
+    'installable': True
+}
