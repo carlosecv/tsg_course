c8a5e844f71d97516f5fcd38a68b5a36360f5b24
Victor Escobar <vescobar@tgsdemx.com>
2026-02-25 17:06:07 -0600

    [IMP] Offers View

--- rs_realstate/__manifest__.py
+++ rs_realstate/__manifest__.py
@@ -26,6 +26,7 @@ Long description of module's purpose
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
         'views/estate_property_type_views.xml',
+        'views/estate_property_offer_views.xml',
         'views/templates.xml',
         'data/ir_sequence.xml',
     ],
